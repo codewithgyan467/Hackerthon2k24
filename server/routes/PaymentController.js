8883220efe9c4588714ac833e94a3885ac364886
Gyandeep Duwarah <143277095+codewithgyan467@users.noreply.github.com>
2024-11-01 13:04:44 +0530

    Update PaymentController.js

--- server/routes/PaymentController.js
+++ server/routes/PaymentController.js
@@ -4,8 +4,7 @@ const asyncHandler = require('express-async-handler');
 exports.processPayment = asyncHandler(async (req, res) => {
     const { amount } = req.body;
 
-    // Implement payment processing logic here (e.g., integrating with a payment gateway)
-    // For example, you can use Stripe or PayPal SDKs
+    // Implement payment processin
 
     res.json({ message: 'Payment processed successfully', amount });
 });
